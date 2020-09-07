70281b0a32a79c90a954b66039994e942932164a
Cường Vũ Quang <cophongsuongbn@gmail.com>
2020-09-07 14:24:57 +0700

    first commit

---
+++ Chapter1/Bai1/1.py
@@ -0,0 +1,20 @@
+number=100
+print(number)
+number_class=12
+print(number_class)
+_64number=100
+print(_64number)
+ten="cuong vu quang"
+print(ten)
+print(type(ten), id(ten))
+
+num=1856
+print(type(num), id(num))
+so=1856
+print(type(so), id(so))
+ten1="cuong"
+ten2="cuong"
+print(id(ten1),id(ten2))
+
+giatri=True
+print(type(giatri))
---
+++ Chapter1/Bai2/1.py
@@ -0,0 +1,25 @@
+n1=900
+print(type(n1))
+n1=10000428386326323232736732732838932899328723723732782
+print(type(n1))
+s=2**2 #dấu mũ
+print(s)
+s=s+1
+print(s)
+s1=5*10+89
+print(s1)
+s2=5.5*139438443572882937457+89.9
+print(s2)
+print(type(s2),id(s2))
+
+c1=80
+c2=115.5
+c=c1+c2
+print(c, type(c),id(c))
+
+c1=13
+c2=5
+c=c1%c2 # chia lấy dư
+print(c)
+print(c1/c2)
+print(c1//c2) #chia làm tròn xuống
